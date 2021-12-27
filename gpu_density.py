ecfde0ebfd152cbcedc5240388584277a2c4060c
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2021-12-26 20:15:25 -0500

    Check that checkpointing file exists before deleting them

--- gpu_density.py
+++ gpu_density.py
@@ -170,7 +170,7 @@ def chkpt_clean(dpath, device_id):
     """ Remove checkpointing file """
     fname = f'device{device_id}_checkpoint.npz'
     fpath = os.path.join(dpath, fname)
-
+    if os.path.isfile(fpath):
     os.remove(fpath)
 
 def _gpu_density(

Fix computation of FWHM for concatenated time series
For a time series that is the result of `n_seg` concatenated segments,
the autocorrelation needs to be computed on each segment separately.
Also, the computation of FWHM now consider the case where the
autocorrelation is monotonically increasing from the start to the global
maxima, i.e., there are no side lobes, only one broad main lobe centered
at the global maximizer.

core.py
@@ -215,30 +215,58 @@ def get_pkg_name():  # pragma: no cover
     """
     return __name__.split(".")[0]
 
-@njit
-def rolling_window(a, window):
-    """
+
+@njit(parallel=True)
+def rolling_window(a, window, splice=None):
+    """    
     Use strides to generate rolling/sliding windows for a numpy array.
 
     Parameters
     ----------
-    a : numpy.ndarray
+    a: numpy.ndarray
         numpy array
-
     window : int
         Size of the rolling window
+    splice: numpy.ndarray
+        Is `splice` is an array, `a` is the result of `n` concatenated 
+        segments. `splice` has the start index of second to last segments. In 
+        other words, the indices where the concatenation of segments takes 
+        place. The rolling windows are extracted for each segment separately 
+        and then vertically stacked.  XXX: `a` is assumed to be a 1D array. 
 
     Returns
     -------
     output : numpy.ndarray
         This will be a new view of the original input array.
+
+    Note:
+    We are JIT compiling this because it is used in ndxcorr, which is jitted.
     """
-    a = np.asarray(a)
-    shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
-    strides = a.strides + (a.strides[-1],)
 
-    return np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+    if splice is not None:
+        start = np.concatenate((np.array([0]), splice))
+        end = np.hstack((splice, np.array([a.size])))
+        n_seg = start.size
+        n_win = end-start-(window-1)
+        cum_win = np.cumsum(n_win)
+        idx = np.hstack((np.array([0]), cum_win))
+        tot_win = np.sum(n_win)
+        out = np.zeros((tot_win, window))
+        for i in prange(n_seg):
+            shape = a[start[i]:end[i]].shape[:-1] + \
+                (a[start[i]:end[i]].shape[-1] - window + 1, window)
+            strides = a[start[i]:end[i]].strides +\
+                (a[start[i]:end[i]].strides[-1],)
+            out[idx[i]:idx[i+1]] = np.lib.stride_tricks.as_strided(
+                a[start[i]:end[i]], shape=shape, strides=strides)
 
+    else:
+        a = np.asarray(a)
+        shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+        strides = a.strides + (a.strides[-1],)
+        out = np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)
+
+    return out
 
 def z_norm(a, axis=0):
     """
@@ -1920,22 +1948,41 @@ def fwhm(x):
     return fwhm
 
 
+def fill_fwhm(fwhm, splice, m):
+    """ Extend fwhm to fill the gaps created by the splice
+
+    `fwhm` is the output from `ndxcorr()`. Its length is smaller than what is 
+    expected in `gpu_density` and `gpu_qsmp` because the subsequences (windows) 
+    that contain the splice are ignored. Here we extend `fwhm` to get the rigth length, adding an arbitrary value for those subsequences. For each segment 
+    in the time series, there are m-1 subsequences that are ignored, with `m`  being the length of a subsequence.
+    """
+    I = np.arange(fwhm.size)
+    splice_ext = np.r_[0, splice, I[-1]+(m-1)*splice.size]
+    n_seg = splice_ext.size - 1
+    new_len = splice_ext[-1] + 1
+    ext_fwhm = np.ones(new_len, dtype=np.int64)
+    for i in range(n_seg):
+        idx = (I >= splice_ext[i]) & (I < splice_ext[i+1])
+        ext_idx = I[idx] + (m-1)*i
+        ext_fwhm[ext_idx] = fwhm[I[idx]]
+
+    return ext_fwhm
+
 @njit(parallel=True, fastmath=True)
-def ndxcorr(T, m):
+def ndxcorr(T, m, splice=None):
     """
     np.correlate() in Numba only allows for the first two arguments, using the 
     'valid' mode by default. So, we need to do the zero padding of each window 
-    in T to get its autocorrelation function. floor(m/2) zeros are padded to left and the remaining zeros (to sum m) are padded to the right. This is done to have the max peak of the autocorrelation at the center of the sequence.
-    Assumes that T has only one dimension.
+    in T to get its autocorrelation function. floor(m/2) zeros are padded to left and the remaining zeros (to sum m) are padded to the right. This is done to have the max peak of the autocorrelation at the center of the sequence. Assumes that T has only one dimension. If `splice` is an array, `T` is the result of concatenating `n_seg` segments, and `splice` contains the indices where the concatenation occurs: the start indices of the second to the last segment; in this case, it computes the autocorrelation only for windows that don't fall at the splice. 
     """
-    X = rolling_window(T, m)
+    X = rolling_window(T, m, splice)
     n, m = X.shape
     xcorr = np.zeros((n, m))
-    xzpadded = np.zeros(2*m - 1, X.dtype)
     l = int(m/2)
     u = l + m
     for i in prange(n):
-        xzpadded[l:u] = X[i].copy()
+        xzpadded = np.zeros(2*m - 1, X.dtype)
+        xzpadded[l:u] = X[i]
         xcorr[i] = np.correlate(X[i], xzpadded)
     return xcorr
 

gpu_qsmp.py
@@ -406,7 +406,8 @@ def gpu_qsmp(T, m, density, dpath, transform=None, splice=None, device_id=0):
     T, M_T, Σ_T = core.preprocess(T, m)
     
     if transform == 'fwhm':
-        fwhm = core.fwhm(core.ndxcorr(T, m))
+        fwhm = core.fwhm(core.ndxcorr(T, m, splice))
+        fwhm = core.fill_fwhm(fwhm, splice, m)
     else:
         fwhm = np.full(0, 0)
 
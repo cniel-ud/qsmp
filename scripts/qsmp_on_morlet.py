eba78f43da72fa00c9c07ee0f78d6783c96886d6
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2022-11-08 09:09:46 -0500

    Clean code

--- scripts/qsmp_on_morlet.py
+++ scripts/qsmp_on_morlet.py
@@ -103,7 +103,8 @@ if compute_density:
 
 # Compute QSMP and indices
 profile, indices = gpu_qsmp(
-    T, sublen, minfilt_size, density, root, splice=splice, device_id=device_ids)
+    T, sublen, minfilt_size, density, root, splice=splice, device_id=device_ids
+)
 
 # Find global maxima (root), and fix neighbor and profile
 profile, indices, density = utils.fix_root((profile, indices, density))

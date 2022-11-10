fde79f60c68ef11885eb1ec206d4084d9b21e2a6
Carlos H. Mendoza-Cardenas <cmendoza@udel.edu>
2022-11-10 15:00:21 -0500

    Save morlet in *.npz file

--- scripts/gen_morlet_signal.py
+++ scripts/gen_morlet_signal.py
@@ -4,41 +4,25 @@ import matplotlib.pyplot as plt
 from spectrum import MultiTapering
 from qsmp.datasets import morlet_signal
 from pathlib import Path
-# %% Generate signal from Morlet wavelets
-freqs = np.array([1, 5, 12, 30, 100, 150])
-sig, freq, freq_cnts = morlet_signal(freqs)
-plt.plot(sig)
-#%%
-root = '/home/cmendoza/software/qsmp-python'
-fname = Path(root).joinpath('data', 'morlet_signal_fs-512.txt')
-np.savetxt(fname, sig, delimiter='\n')
-#%%
-sig = np.loadtxt(fname)
-#%%
 
+#%% Generate signal from Morlet wavelets
+sig_params = dict(
+    fs=512,
+    wave_len=512,
+    n_waves=1000,
+    noise_std=0.07,
+    pmf_exp=3.1
+)
+freqs = np.array([1, 5, 12, 30, 100, 150])
+sig, freq, freq_cnts, SNRdB = morlet_signal(freqs, **sig_params)
+plt.plot(sig[13*sig_params['wave_len']:20*sig_params['wave_len']])
+#%% Save data
+root = '/home/cmendoza/software/qsmp/data/morlet'
+fpath = Path(root).joinpath('morlet_signal.npz')
+with fpath.open('wb') as f:
+    np.savez(
+        f, T=sig, splice=None, **sig_params
+    )
+#%%
 X = MultiTapering(sig, NW=3, sampling=512)
 X.plot()
-PSD, f = X.psd, X.frequencies()
-imax = np.argmax(PSD)
-f[imax]
-# %%
-wnames = ['db4']
-for wname in wnames:
-    wavelet = pywt.Wavelet(wname)
-    phi, psi, x = wavelet.wavefun(level=9)
-    plt.figure()
-    plt.plot(x, psi)
-    plt.title(wname)
-# %%
-cfreq = pywt.central_frequency('db4', precision=9)
-y = np.cos(2*np.pi*cfreq*x+3*np.pi/4)
-plt.plot(x, psi)
-plt.plot(x, y)
-#%%
-Psi = MultiTapering(psi, NW=3, sampling=512)
-Psi.plot()
-#%%
-PSD, f = Psi.psd, Psi.frequencies()
-imax = np.argmax(PSD)
-f[imax]
-# %%
